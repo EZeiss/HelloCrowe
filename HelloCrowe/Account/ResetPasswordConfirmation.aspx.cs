bb41b7b7169df6298510ebec630728cdf69658cf
EZeiss <34254472+EZeiss@users.noreply.github.com>
2017-12-04 17:42:10 -0600

    Add project files.

---
+++ HelloCrowe/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace HelloCrowe.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ HelloCrowe/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloCrowe.Startup))]
+namespace HelloCrowe
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
